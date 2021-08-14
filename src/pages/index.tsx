b331237318c5863d95c1790e4b342929306ec34e
Thanh Minh <thanhgamedev@gmail.com>
2021-08-14 09:30:02 +0000

    feat: add index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
-import type { NextPage } from 'next'
+import { getDefaultLayout } from '@/layouts/DefaultLayout'
+import { getItems } from 'src/logic/item'
+import Item from '@/components/Item'
 
-const Home: NextPage = () => {
-  return <div></div>
+export const getStaticProps = async () => {
+  const contents = await getItems()
+  return {
+    props: {
+      contents,
+    },
+  }
 }
+
+const Home = ({ contents }: any) => {
+  return (
+    <>
+      <div className="flex flex-col mt-4">
+        <div className="grid grid-cols-3 gap-2">
+          {contents.map((content: any, index: number) => (
+            <Item
+              key={index}
+              title={content.Title}
+              category={content.Category}
+              description={content.Description}
+              link={content.Link}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+Home.getLayout = getDefaultLayout
 
 export default Home
