6234136124d5ef33705da82cf5e20b5867c47335
Thanh Minh <thanhgamedev@gmail.com>
2021-08-14 15:18:40 +0000

    feat: update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
+import Link from 'next/link'
+
 const Navbar = () => {
   return (
     <div className="flex w-full px-56 py-8 bg-white min-h-20">
       <div className="flex items-center w-1/3 space-x-4">
-        <h1 className="text-xl font-bold">Home</h1>
+        <Link href="/" passHref>
+          <a className="text-xl font-bold">Home</a>
+        </Link>
         <h1 className="text-xl font-bold">Cheatsheet</h1>
         <h1 className="text-xl font-bold">Font generator</h1>
       </div>
       <div className="flex items-center justify-center w-1/3">
-        <h1 className="text-4xl font-bold uppercase">AWSNT</h1>
+        <Link href="/" passHref>
+          <a className="text-4xl font-bold uppercase">AWSNT</a>
+        </Link>
       </div>
       <div className="flex items-center justify-end w-1/3">
         <h1 className="text-xl font-bold">About</h1>
