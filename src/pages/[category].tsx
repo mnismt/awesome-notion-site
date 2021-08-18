26d324b3e906c4feb9a7caeaa0ae99e6c79ff761
Thanh Minh <thanhgamedev@gmail.com>
2021-08-18 02:47:45 +0000

    feat: show all tags of category

--- src/pages/[category].tsx
+++ src/pages/[category].tsx
@@ -2,6 +2,8 @@ import Item from '@/components/Item'
 import { getDefaultLayout } from '@/layouts/DefaultLayout'
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 import { Content, getCategoriesName, getItems } from '@/logic/item'
+import { useConfigStore } from 'src/store'
+import Badge from '@/components/Badge'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const categories = await getCategoriesName()
@@ -17,19 +19,35 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }: any) => {
   const category = params.category
   const contents: Array<Content> = await getItems(category)
+  const tagsList = contents.map((content) => content.Tags).flat()
+  const tags = tagsList.filter((tag, index) => tagsList.indexOf(tag) === index)
   return {
     props: {
       contents,
+      tags,
     },
   }
 }
 
 const Category = ({
   contents,
+  tags,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const currentCategory = useConfigStore((state) =>
+    state.getCurrentActiveCategory()
+  )
   return (
     <>
-      <div className="flex flex-col mt-4 space-y-10">
+      <div className="flex flex-col mt-4 space-y-5">
+        <hr className="w-full border-black" />
+        <h1 className="text-2xl text-center">
+          {currentCategory && currentCategory.description}
+        </h1>
+        <div className="flex items-center justify-center space-x-2">
+          {tags.map((tag: string, index: number) => (
+            <Badge key={index} text={tag} />
+          ))}
+        </div>
         <div className="grid grid-cols-3 gap-4">
           {contents.map((content: Content, index: number) => (
             <Item key={index} {...content} />
