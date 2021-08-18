aa86acb596373c706e2edec5b146841f540c21e2
Thanh Minh <thanhgamedev@gmail.com>
2021-08-18 03:24:50 +0000

    style: add custom pointer in badge component

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,6 +1,12 @@
-const Badge = ({ text }: { text: string }) => {
+const Badge = ({ text, pointer }: { text: string; pointer?: boolean }) => {
   return (
-    <span className="px-2 py-1 border-2 border-black rounded-xl">{text}</span>
+    <span
+      className={`px-2 py-1 border-2 border-black rounded-xl ${
+        pointer && 'cursor-pointer'
+      }`}
+    >
+      {text}
+    </span>
   )
 }
 
