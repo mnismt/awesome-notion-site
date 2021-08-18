b6a96fa34a2d09a5d255f999690aec2c7a1a04c0
Thanh Minh <thanhgamedev@gmail.com>
2021-08-18 03:35:21 +0000

    style: update badge component

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,7 +1,7 @@
 const Badge = ({ text, pointer }: { text: string; pointer?: boolean }) => {
   return (
     <span
-      className={`px-2 py-1 border-2 border-black rounded-xl ${
+      className={`px-2 py-1 border-2 border-black rounded-xl hover:scale-105 transform duration-500 ${
         pointer && 'cursor-pointer'
       }`}
     >
