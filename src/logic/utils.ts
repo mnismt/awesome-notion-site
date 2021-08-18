40271a6d9a5d2487c6bdd15a839a8ada0431ca0e
Thanh Minh <thanhgamedev@gmail.com>
2021-08-18 08:07:55 +0000

    feat: update category page

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -4,3 +4,12 @@ export const removeDuplicateElements = <T>(array: Array<T>) => {
 
 export const capitalizeFirstLetter = (text: string) =>
   text.charAt(0).toUpperCase() + text.slice(1)
+
+export const getDefaultVariants = (delay: number) => ({
+  visible: (i: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: i * delay },
+  }),
+  hidden: { opacity: 0, y: -10 },
+})
--- src/pages/[category].tsx
+++ src/pages/[category].tsx
@@ -2,10 +2,11 @@ import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 import Link from 'next/link'
 import Item from '@/components/Item'
 import { Content, getCategoriesName, getItems } from '@/logic/item'
-import { useConfigStore } from 'src/store'
 import Badge from '@/components/Badge'
-import { removeDuplicateElements } from '@/logic/utils'
-import { motion } from 'framer-motion'
+import { getDefaultVariants, removeDuplicateElements } from '@/logic/utils'
+import { AnimatePresence, motion } from 'framer-motion'
+import { getCategoryLayout } from '@/layouts/CategoryLayout'
+import { useConfigStore } from 'src/store'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const categories = await getCategoriesName()
@@ -26,48 +27,90 @@ export const getStaticProps: GetStaticProps = async ({ params }: any) => {
   )
   return {
     props: {
+      category,
       contents,
       tags,
     },
   }
 }
 
+const AnimationWrapper = ({
+  delay,
+  children,
+  className,
+}: {
+  delay: number
+  children: JSX.Element
+  className: string
+}) => {
+  const variants = getDefaultVariants(0)
+  return (
+    <motion.div
+      variants={variants}
+      animate={{ opacity: 1, y: 0, transition: { delay } }}
+      initial="hidden"
+      exit="hidden"
+      className={className}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
 const Category = ({
+  category,
   contents,
   tags,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const currentCategory = useConfigStore((state) =>
     state.getCurrentActiveCategory()
   )
+  const variants = getDefaultVariants(0.3)
+
   return (
-    <>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        className="flex flex-col mt-4 space-y-5"
+    <motion.div
+      key={category}
+      className="flex flex-col space-y-5"
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
+      <AnimatePresence exitBeforeEnter>
+        {currentCategory && (
+          <motion.h1
+            key={currentCategory.id}
+            variants={variants}
+            animate="visible"
+            initial="hidden"
+            exit="hidden"
+            className="text-2xl text-center font-bold"
+          >
+            {currentCategory.description}
+          </motion.h1>
+        )}
+      </AnimatePresence>
+      <AnimationWrapper
+        className="flex items-center justify-center space-x-2"
+        delay={0.3}
       >
-        <hr className="w-full border-black" />
-        <h1 className="text-2xl text-center">
-          {currentCategory && currentCategory.description}
-        </h1>
-        <div className="flex items-center justify-center space-x-2">
-          {tags.map((tag: string, index: number) => (
-            <Link key={index} href={`/tag/${tag.toLowerCase()}`} passHref>
-              <div>
-                <Badge text={tag} pointer />
-              </div>
-            </Link>
-          ))}
-        </div>
-        <div className="grid grid-cols-3 gap-4">
-          {contents.map((content: Content, index: number) => (
-            <Item key={index} {...content} />
-          ))}
-        </div>
-      </motion.div>
-    </>
+        {tags.map((tag: string, index: number) => (
+          <Link key={index} href={`/tag/${tag.toLowerCase()}`} passHref>
+            <div>
+              <Badge text={tag} pointer />
+            </div>
+          </Link>
+        ))}
+      </AnimationWrapper>
+      <AnimationWrapper delay={0.5} className="grid grid-cols-3 gap-4">
+        {contents.map((content: Content, index: number) => (
+          <Item key={index} {...content} />
+        ))}
+      </AnimationWrapper>
+    </motion.div>
   )
 }
+
+Category.getLayout = getCategoryLayout
 
 export default Category
