9aab0bede448b500e07109bfbdc11f240407c890
Thanh Minh <thanhgamedev@gmail.com>
2021-08-18 04:11:41 +0000

    feat: update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,21 @@ const Navbar = () => {
   return (
     <div className="flex w-full px-56 py-8 bg-white min-h-20">
       <div className="flex items-center w-1/3 space-x-4">
-        <Link href="/" passHref>
-          <a className="text-xl font-bold">Home</a>
-        </Link>
+        <a
+          className="text-xl font-bold"
+          href="https://mnismt.notion.site/About-3d0a7bf942ad42a3b4492e4ab8c4e4ca"
+          target="_blank"
+        >
+          About
+        </a>
       </div>
       <div className="flex items-center justify-center w-1/3">
         <Link href="/" passHref>
-          <a className="text-4xl font-bold uppercase">AWSNT</a>
+          <a className="text-5xl font-bold uppercase">AWSNT</a>
         </Link>
       </div>
       <div className="flex items-center justify-end w-1/3">
-        <h1 className="text-xl font-bold">About</h1>
+        <h1 className="text-xl font-bold">Submit</h1>
       </div>
     </div>
   )
