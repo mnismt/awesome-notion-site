f5c96c7ae0492ca625f40d059659311e6859af5c
Thanh Minh <thanhgamedev@gmail.com>
2021-08-18 08:08:09 +0000

    feat: update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { InferGetStaticPropsType } from 'next'
 import { Content, getItems } from 'src/logic/item'
 import Item from '@/components/Item'
+import { getDefaultVariants } from '@/logic/utils'
+import { motion } from 'framer-motion'
 
 export const getStaticProps = async () => {
   const contents: Array<Content> = await getItems()
@@ -12,15 +14,22 @@ export const getStaticProps = async () => {
 }
 
 const Home = ({ contents }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const variants = getDefaultVariants(0)
   return (
     <>
-      <div className="flex flex-col mt-4">
+      <motion.div
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        className="flex flex-col mt-4"
+      >
         <div className="grid grid-cols-3 gap-4">
           {contents.map((content, index: number) => (
             <Item key={index} {...content} />
           ))}
         </div>
-      </div>
+      </motion.div>
     </>
   )
 }
