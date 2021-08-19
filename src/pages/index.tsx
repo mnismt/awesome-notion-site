d1de88bfb91128f08f80ea0966068b879d1e5868
Thanh Minh <thanhgamedev@gmail.com>
2021-08-19 02:27:30 +0000

    feat: update the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import { InferGetStaticPropsType } from 'next'
-import { Content, getItems, getTags } from 'src/logic/item'
-import Item from '@/components/Item'
+import ContentBox from '@/components/ContentBox'
+import { motion } from 'framer-motion'
+
+import { Content, getItems } from 'src/logic/item'
 import { getDefaultVariants } from '@/logic/utils'
-import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion'
-import { useState, useEffect } from 'react'
-import { search } from 'ss-search'
-import Badge from '@/components/Badge'
-import SearchBox from '@/components/SearchBox'
 
 export const getStaticProps = async () => {
   const contents: Array<Content> = await getItems()
@@ -18,26 +15,7 @@ export const getStaticProps = async () => {
 }
 
 const Home = ({ contents }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const variants = getDefaultVariants(0.05)
-  const [items, setItems] = useState<Content[]>(contents)
-  const [tags, setTags] = useState<Array<string>>()
-  const [showTag, setShowTag] = useState<boolean>(false)
-  const [enableAnimation, setEnableAnimation] = useState<boolean>(false)
-  const [searchText, setSearchText] = useState('')
-  useEffect(() => {
-    setEnableAnimation(false)
-    const results = search(
-      contents,
-      // search keys
-      ['Description', 'Type', 'Tags', 'Category', 'Title'],
-      searchText
-    )
-    setItems(results as Content[])
-  }, [searchText])
-  useEffect(() => {
-    setTags(items.map((item) => item.Tags).flat())
-    setEnableAnimation(true)
-  }, [items])
+  const variants = getDefaultVariants(0.3)
   return (
     <>
       <motion.div
@@ -47,53 +25,7 @@ const Home = ({ contents }: InferGetStaticPropsType<typeof getStaticProps>) => {
         exit="hidden"
         className="flex flex-col mt-4 space-y-4"
       >
-        <div className="flex flex-col items-center justify-center px-56">
-          <SearchBox setSearchText={setSearchText} />
-        </div>
-        <div className="flex flex-col space-y-4">
-          <div className="flex items-center justify-center">
-            <button
-              className="h-2 w-12 bg-gray-200 hover:bg-black transition duration-300 rounded-xl"
-              onClick={(_) => setShowTag(!showTag)}
-            ></button>
-          </div>
-          <AnimateSharedLayout>
-            <AnimatePresence exitBeforeEnter>
-              {showTag && (
-                <motion.div
-                  layout={enableAnimation}
-                  variants={variants}
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  className="flex items-center justify-center flex-wrap"
-                >
-                  {tags?.map((tag, index) => (
-                    <motion.div
-                      key={index}
-                      custom={index}
-                      variants={variants}
-                      initial="hidden"
-                      animate="visible"
-                      exit="hidden"
-                      className="py-2 px-1"
-                    >
-                      <Badge text={tag} link={`/tag/${tag.toLowerCase()}`} />
-                    </motion.div>
-                  ))}
-                </motion.div>
-              )}
-            </AnimatePresence>
-            <motion.div
-              layout={enableAnimation}
-              className="grid grid-cols-3 gap-4"
-            >
-              {items.map((content, index: number) => (
-                <Item key={index} {...content} />
-              ))}
-            </motion.div>
-          </AnimateSharedLayout>
-        </div>
+        <ContentBox contents={contents} />
       </motion.div>
     </>
   )
