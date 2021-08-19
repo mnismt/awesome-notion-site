d97663823e4bafb19d3202fe2abb6674e65a2315
Thanh Minh <thanhgamedev@gmail.com>
2021-08-19 16:01:20 +0000

    feat: update env

--- fetchConfig.js
+++ fetchConfig.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const request = require('request')
 const path = require('path')
 const categoriesDatabaseId = '741a0da234f64a7e8404ae8d433b26e4'
-const env = fs.readFileSync(path.join(__dirname, '/.env.local'), 'utf-8')
+const env = fs.readFileSync(path.join(__dirname, '/.env'), 'utf-8')
 const WORKER_ENDPOINT = env.replace('WORKER_ENDPOINT=', '')
 
 request
