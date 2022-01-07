8b10a85dac468fa43dc3006b9a088cbcf7775a5e
Thanh Minh <thanhgamedev@gmail.com>
2022-01-07 21:25:56 +0700

    feat: remove mocks

--- src/logic/item.ts
+++ src/logic/item.ts
@@ -18,34 +18,19 @@ export interface Category {
 }
 
 const WORKER_ENDPOINT = process.env.WORKER_ENDPOINT
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-// dynamic import mocks data
-const getItemsMock = () =>
-  import('@/mocks/items.json').then((module) => module.default)
 
 export const getItems = async (category?: string) => {
-  let contents: Array<Content>
-  // If in dev environment, fetching data from mocks will be faster
-  if (isDevelopment) {
-    contents = await getItemsMock()
-  } else {
-    const res = await fetch(
-      `${WORKER_ENDPOINT}/v1/table/82de766385fc439fbb010d9cf01e075b`
-    )
-    contents = await res.json()
-  }
-  if (category)
-    return contents.filter((content) => content.Category === category)
+  let contents: Array<Content> = await fetch(`${WORKER_ENDPOINT}/v1/table/82de766385fc439fbb010d9cf01e075b`).then(
+    (res) => res.json()
+  )
+  if (category) return contents.filter((content) => content.Category === category)
   return contents
 }
 
 export const getItemsByTag = async (tag: string) => {
   const items = await getItems()
   // lowercase tags before match
-  return items.filter((item) =>
-    item.Tags.map((tag) => tag.toLowerCase()).includes(tag)
-  )
+  return items.filter((item) => item.Tags.map((tag) => tag.toLowerCase()).includes(tag))
 }
 
 export const getCategories = () => {
